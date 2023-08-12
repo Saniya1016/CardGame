0ad9133bcdbcba0dd45fdd22be790d1d5e75b4f2
Saniya Sekhon <ssekhon@umass.edu>
2023-08-12 00:49:18 -0400

    Added textboc for name

--- client/src/Landing.js
+++ client/src/Landing.js
@@ -1,13 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
 import './Landing.css';
 
 //Landing page to start the game
 export const Landing = ({onStartGame}) => {
+
+    const [name, setName] = useState('');
+
+    const handleNameChange = (event) => {
+        setName(event.target.value);
+      }
+
     return (
         <div className="landing-page">
             <header className="header">
                 <h1>HIGH OR LOW GAME</h1>
             </header>
+            <div>
+                <input
+                    type="text"
+                    placeholder="Enter your name"
+                    value={name}
+                    onChange={handleNameChange}
+                />
+            </div>
             <main className="main">
                 <button onClick={onStartGame}>Start Game</button>
             </main>
